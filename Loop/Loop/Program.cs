d602868fd1ce969dec5c3e0603cecdfcad3bd189
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-01-22 19:17:15 -0500

    Add files via upload

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i=1; i <= 20; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The resule is: " + sum);
+        }
+
+
+
+
+    }
+    }
+
